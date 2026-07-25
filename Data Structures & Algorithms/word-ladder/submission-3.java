89a346ce3986e1541e09a45d42272d3d91930784
Shivam Gupta <69157031+Shivam-gupta-cmyk@users.noreply.github.com>
2026-07-25 18:53:36 +0530

    Add: word-ladder - submission-3

---
+++ Data Structures & Algorithms/word-ladder/submission-3.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean compareWords(String a, String b) {
+        if (a.length() != b.length())
+            return false;
+        int flag = 0;
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != b.charAt(i))
+                flag++;
+            if (flag == 2)
+                return false;
+        }
+        return flag == 1;
+    }
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        if(beginWord.equals(endWord)) return 0;
+        Set<String> visited = new HashSet<>();
+        Queue<String> pq = new ArrayDeque<>();
+        pq.add(beginWord);
+        visited.add(beginWord);
+        int level = 1;
+        while (!pq.isEmpty()) {
+            int size = pq.size();
+            while (size != 0) {
+                String topWord = pq.poll();
+                size--;
+                if(topWord.equals(endWord)){
+                    return level;
+                }
+                for (int i = 0; i < wordList.size(); i++) {
+                    if (!visited.contains(wordList.get(i))
+                        && compareWords(topWord, wordList.get(i))) {
+                            visited.add(wordList.get(i));
+                            pq.add(wordList.get(i));
+                    }
+                }
+            }
+            level++;
+        }
+        return 0;
+    }
+}
