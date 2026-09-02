254a7d89e9c13f7addecf9b660a8893f277f2d07
Shivam Gupta <69157031+Shivam-gupta-cmyk@users.noreply.github.com>
2026-09-03 00:19:09 +0530

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.java
@@ -0,0 +1,22 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        Collections.sort(intervals,(a,b)-> Integer.compare(a.start,b.start));
+        int endTime = -1;
+        for(Interval i : intervals){
+            if(i.start<endTime) return false;
+            endTime = i.end;
+        }
+        return true;
+    }
+}
